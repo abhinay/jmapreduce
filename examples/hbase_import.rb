418a3973c9d1dc73f188c9b593f68720a3a29114
Abhinay Mehta <abhinay.mehta@gmail.com>
2011-09-13 12:13:01 +0100

    Added hbase simple example and conf example

--- examples/hbase_import.rb
+++ examples/hbase_import.rb
@@ -1,62 +1,19 @@
 import org.fingertap.jmapreduce.JMapReduce
 
 import org.apache.hadoop.hbase.client.Put
-import org.apache.hadoop.hbase.client.HBaseAdmin
-
-import org.apache.hadoop.hbase.HTableDescriptor
-import org.apache.hadoop.hbase.HColumnDescriptor
 import org.apache.hadoop.hbase.HBaseConfiguration
 
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil
 
-JMapReduce.job "Calculating adgroup stats for keywords for #{JMapReduce.property('client')}" do
-  map_tasks 20
-  reduce_tasks 5
-  
-  setup do
-    HEADERS = property('csv_headers').split(":")
-  end
-  
-  map do |key, value|
-    row = [key] + value.split("\t")
-    keyword = row[HEADERS.index('keyword')]
-    adgroup_id = row[HEADERS.index('adgroup_id')]
-    
-    stats = %w(clicks cpc ctr avg_position quality_score).inject({}) do |h,column|
-      h[column] = row[HEADERS.index(column)]
-      h
-    end
-    
-    emit(keyword, { adgroup_id => stats })
-  end
-  
-  reduce do |keyword, values|
-    keyword_stats = {}
-    stat_rows = {}
-    max_clicks = -1
-    
-    values.each do |adgroup_stats|
-      adgroup_stats.each do |(adgroup_id,stats)|
-        keyword_stats = stats if stats['clicks'].to_i > max_clicks
-        stats.keys.each do |key|
-          stat_rows["#{adgroup_id}:#{key}"] = stats[key]
-        end
-      end
-    end
-    
-    emit(keyword, keyword_stats.merge(stat_rows))
-  end
-end
-
-JMapReduce.job "Keywords bulk import for #{JMapReduce.property('client')}" do
+JMapReduce.job "HBase bulk import job" do
   reduce_tasks 0
   
   custom_job do |conf|
     hbase_conf = HBaseConfiguration.create(conf)
-    hbase_conf.set('hbase.zookeeper.quorum', 'hbase-master.hadoop.forward.co.uk')
-    job = Job.new(hbase_conf, "#{property('client')} keywords bulk import")
-
+    hbase_conf.set('hbase.zookeeper.quorum', 'hbase.server.address')
+    job = Job.new(hbase_conf, "HBase bulk import job")
+    
     TableMapReduceUtil.initTableReducerJob(property('table_name'), nil, job)
     TableMapReduceUtil.addDependencyJars(job)
     TableMapReduceUtil.addDependencyJars(job.getConfiguration)
@@ -65,36 +22,26 @@ JMapReduce.job "Keywords bulk import for #{JMapReduce.property('client')}" do
     job
   end
   
-  before_job do |job|
-    hba = HBaseAdmin.new(job.getConfiguration)
-    unless hba.tableExists(property('table_name'))
-      ht = HTableDescriptor.new("fab_abs")
-      ht.addFamily(HColumnDescriptor.new("stats"))
-      hba.createTable(ht)
-    end
-  end
-  
   setup do
-    @family = "stats".to_java_bytes
+    @family = "someColumnFamily".to_java_bytes
     @ts = java.lang.System.currentTimeMillis
-    @headers = property('csv_headers').split(':')
   end
   
-  map do |keyword, stats|
-    keyBytes = keyword.to_java_bytes
-    rowKey = ImmutableBytesWritable.new(keyBytes)
-    put = Put.new(keyBytes)
+  map do |key, value|
+    row = "#{key}\t#{value}".split("\t")
     
-    stats.each do |(qualifier,stat)|
-      put.add(@family, qualifier.to_java_bytes, @ts, stat.to_s.to_java_bytes)
-    end
+    row_key = row[0].to_java_bytes
+    someColumn = row[1].to_java_bytes
+    someOtherColumn = row[2].to_java_bytes
     
-    context.write(rowKey, put)
+    put = Put.new(row_key)
+    put.add(@family, "columnName".to_java_bytes, @ts, someColumn)
+    put.add(@family, "anotherColumnName".to_java_bytes, @ts, someOtherColumn)
+    
+    context.write(ImmutableBytesWritable.new(row_key), put)
   end
 end
 
 __END__
 
-mandy-rm /tmp/output -c ../cardwall-alerts/config/production/hadoop_cluster.xml && ./bin/jmapreduce examples/hbase_import.rb /user/hive/warehouse/ask_keywords/dated=2011-09-06/client=ask_pt /tmp/output -l /Users/abhinay/tools/hbase-0.90.3-cdh3u1/hbase-0.90.3-cdh3u1.jar,/Users/abhinay/tools/hbase-0.90.3-cdh3u1/lib/zookeeper-3.3.3-cdh3u1.jar,/Users/abhinay/tools/hbase-0.90.3-cdh3u1/lib/guava-r06.jar -v csv_headers=account:campaign:ad_group:keyword_id:keyword:match_type:status:first_page_bid:quality_score:distribution:max_cpc:destination_url:ad_group_status:campaign_status:currency_code:impressions:clicks:ctr:cpc:cost:avg_position:account_id:campaign_id:adgroup_id,table_name=abs_test_keywords,client=ask_pt -c ../cardwall-alerts/config/production/hadoop_cluster.xml
-
-rm -rf /tmp/output* && ./bin/jmapreduce examples/hbase_import.rb /Users/abhinay/Desktop/data.tsv /tmp/output -l /Users/abhinay/tools/hbase-0.90.3-cdh3u1/hbase-0.90.3-cdh3u1.jar,/Users/abhinay/tools/hbase-0.90.3-cdh3u1/lib/zookeeper-3.3.3-cdh3u1.jar,/Users/abhinay/tools/hbase-0.90.3-cdh3u1/lib/guava-r06.jar -v csv_headers=account:campaign:ad_group:keyword_id:keyword:match_type:status:first_page_bid:quality_score:distribution:max_cpc:destination_url:ad_group_status:campaign_status:currency_code:impressions:clicks:ctr:cpc:cost:avg_position:account_id:campaign_id:adgroup_id,table_name=abs_test_keywords,client=ask_pt
+./bin/jmapreduce examples/hbase_import.rb /path/to/tsv/file /output/path -l $HBASE_HOME/hbase.jar,$HBASE_HOME/lib/zookeeper.jar,$HBASE_HOME/lib/guava.jar -v table_name=test_import_table
